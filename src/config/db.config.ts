8c35d29ca0044297ed737bbfd999afa02a9ef6df
imykhailychenko <igor.c.m@ukr.net>
2021-10-23 16:46:49 +0300

    config prod run

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,15 +6,15 @@ import { UserEntity } from '../api/users/entities/users.entity';
 
 const dbConfig: ConnectionOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.POSTGRES_HOST || 'localhost',
     database: process.env.POSTGRES_DB || 'genesis-hackathon',
     port: +process.env.POSTGRES_PORT || 5432,
     username: process.env.POSTGRES_USER || 'genesis-hackathon',
     password: process.env.POSTGRES_PASSWORD || 'genesis-hackathon',
     logging: false,
-    synchronize: true,
-    migrationsRun: true,
     entities: [UserEntity, PostEntity, FavoriteEntity],
+    ssl: Boolean(process.env.DB_SSL),
+    synchronize: true,
 };
 
 export default dbConfig;
