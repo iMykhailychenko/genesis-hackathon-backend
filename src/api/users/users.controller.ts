4c71cd294bd4cd9890d16f8c5a5d8bec481ee01c
imykhailychenko <igor.c.m@ukr.net>
2021-10-23 16:00:21 +0300

    edit Procfile

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,19 +1,18 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import CreateUserDto from './dto/join.dto';
 import { LoginDto } from './dto/login.dto';
 import { LoginInterface } from './users.interface';
 import { UsersService } from './users.service';
 
-@ApiBearerAuth()
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly userService: UsersService) {}
 
     @Post()
-    @ApiOperation({ summary: 'Join (Authorization)' })
+    @ApiOperation({ summary: 'Join' })
     @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'User created' })
     @HttpCode(HttpStatus.NO_CONTENT)
     async join(@Body(new ValidationPipe()) createUserDto: CreateUserDto): Promise<void> {
@@ -21,7 +20,7 @@ export class UsersController {
     }
 
     @Post('login')
-    @ApiOperation({ summary: 'Login (Authorization)' })
+    @ApiOperation({ summary: 'Login' })
     @ApiResponse({ status: HttpStatus.OK, description: 'Ok' })
     async login(@Body(new ValidationPipe({ transform: true })) loginDto: LoginDto): Promise<LoginInterface> {
         return await this.userService.login(loginDto);
