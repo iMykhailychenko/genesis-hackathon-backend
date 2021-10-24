fd46afe32f998da69bfa3ca34e946723649a74dc
imykhailychenko <igor.c.m@ukr.net>
2021-10-24 10:39:12 +0300

    toggle favorite

--- src/api/favorite/favorite.controller.ts
+++ src/api/favorite/favorite.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Param, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+import { User } from '../../shared/decorators/users.decorator';
+import { AuthGuard } from '../../shared/guards/auth.guards';
+
+import { FavoriteService } from './favorite.service';
+
+@ApiTags('favorite')
 @Controller('favorite')
-export class FavoriteController {}
+export class FavoriteController {
+    constructor(private readonly favoriteService: FavoriteService) {}
+
+    @Put(':postId')
+    @ApiOperation({ summary: 'Toggle favorite' })
+    @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'Toggle favorite' })
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async toggleFavorite(@User('id') userId: number, @Param('postId', ParseIntPipe) postId: number): Promise<void> {
+        await this.favoriteService.toggleFavorite(userId, postId);
+    }
+}
--- src/api/favorite/favorite.module.ts
+++ src/api/favorite/favorite.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthGuard } from '../../shared/guards/auth.guards';
+import { PostEntity } from '../posts/entities/posts.entity';
+import { UserEntity } from '../users/entities/users.entity';
 
 import { FavoriteEntity } from './entities/favorite.entity';
 import { FavoriteController } from './favorite.controller';
 import { FavoriteService } from './favorite.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([FavoriteEntity])],
+    imports: [TypeOrmModule.forFeature([FavoriteEntity, PostEntity, UserEntity])],
     controllers: [FavoriteController],
     providers: [FavoriteService, AuthGuard],
 })
